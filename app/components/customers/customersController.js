a987a60c36a9985abf4933bd191efca33c77c934
Amit Eliav <amiteliav1@gmail.com>
2015-11-14 20:35:13 +0200

    Add restaurants json hard-coded

--- app/components/customers/customersController.js
+++ app/components/customers/customersController.js
@@ -5,9 +5,49 @@
 
 myApp.controller('customersCtrl', ['$scope', '$route', '$location',
     function ($scope, $route, $location) {
+
         $scope.dishes = [
             'מסבחה',
             'חומוס',
             'פיתה'
+        ]
+        $scope.restaurants = [
+            {
+                "name": "אבו חסן",
+                "logo": "app/assets/images/masabaha.jpg",
+                "phone": "03-6666666",
+                "address": "מתחת לעץ 6, תל אביב-יפו",
+                "dishes": [{
+                    "name": "מסבחה",
+                    "price": "66"
+                },
+                    {
+                    "name": "חומוס",
+                    "price": "66"
+                },
+                {
+                    "name": "פיתה",
+                    "price": "2"
+                }]
+            },
+            {
+                "name": "דומינוס פיצה",
+                "logo": "app/assets/images/pizza.png",
+                "phone": "03-6666666",
+                "address": "מתחת לעץ 6, תל אביב-יפו",
+                "dishes": [{
+                    "name": "פיצה",
+                    "price": "66"
+                },
+                {
+                    "name": "לחם שום",
+                    "price": "66"
+                },
+                {
+                    "name": "רוטב שום",
+                    "price": "2"
+                }
+                ]
+            }
         ]
 }]);
